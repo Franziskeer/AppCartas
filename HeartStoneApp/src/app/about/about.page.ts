fca36d94c429a8e439d0c0c2f24c7b500adbeb55
Franziskeer <fpa19@alu.ua.es>
2020-04-07 19:37:16 +0200

    Taller 1 de Ionic

---
+++ HeartStoneApp/src/app/about/about.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.page.html',
+  styleUrls: ['./about.page.scss'],
+})
+export class AboutPage implements OnInit {
+
+  readonly appName:string = "HeartStoneApp"
+  readonly releaseDate:string = "07/04/2020"
+  readonly version:string = "v1.0"
+  readonly autor:string = "Francisco Piñero Alpañés"
+  constructor(private platform: Platform) { 
+    this.platform = platform;
+  }
+
+  ngOnInit() {
+  }
+
+  public exitApp() {
+    confirm('Do you want to exit from the app?');
+    navigator['app'].exitApp();
+  }
+}
---
+++ HeartStoneApp/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { ToastController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
+})
+export class AppComponent {
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar,
+    private toastController: ToastController
+  ) {
+    this.initializeApp();
+    this.showToast();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+
+  async showToast() {
+    const toast = await this.toastController.create({
+      message: 'Welcome to HeartStoneApp!',
+      position: 'top',
+      duration: 2000
+    }); 
+    toast.present();
+  }
+}
